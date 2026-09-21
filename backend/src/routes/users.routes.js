d70fb5da6a4601413ef4ca2b1175e3cdce69db0f
Gianfranco Nasuti <gianfranconasuti123@gmail.com>
2026-09-20 23:27:35 -0300

    cambios en billetera y en "mis activdades"

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,6 +1,8 @@
 const express=require('express')
-const {createUser}=require('../controllers/users.controller')
+const{createUser, getAuctionsByUser, getBidsByUser}=require('../controllers/users.controller')
 
 const router=express.Router()
 router.post('/',createUser)
+router.get('/:userId/auctions', getAuctionsByUser)
+router.get('/:userId/bids', getBidsByUser)
 module.exports=router
